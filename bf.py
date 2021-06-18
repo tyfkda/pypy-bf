05d7a3e18080b55f929d5793b9b710e6e686bc9e
tyfkda <tyfkda@gmail.com>
2021-06-18 18:32:34 +0900

    purefunction
    Time: 13.34s => 4.67s

--- bf.py
+++ bf.py
@@ -1,14 +1,28 @@
 try:
   from rpython.rlib.jit import JitDriver
+  from rpython.rlib.jit import purefunction
 except ImportError:
   class JitDriver(object):
     def __init__(self,**kw): pass
     def jit_merge_point(self,**kw): pass
     def can_enter_jit(self,**kw): pass
 
+  def purefunction(f):
+    return f
+
+def get_location(pc, program, bracket_map, tape):
+    return "%s_%s_%s" % (
+      program[:pc], program[pc], program[pc+1:]
+    )
+
 jitdriver = JitDriver(
   greens=['pc', 'program', 'bracket_map', 'tape'],
-  reds=['head'])
+  reds=['head'],
+  get_printable_location=get_location)
+
+@purefunction
+def get_matching_bracket(bracket_map, pc):
+    return bracket_map[pc]
 
 def mainloop(program, bracket_map, putc, getc):
   tape = [0] * 30000
@@ -34,10 +48,12 @@ def mainloop(program, bracket_map, putc, getc):
       tape[head] = getc()
     elif code == '[':
       if tape[head] == 0:
-        pc = bracket_map[pc]
+        #pc = bracket_map[pc]
+        pc = get_matching_bracket(bracket_map, pc)
     elif code == ']':
       if tape[head] != 0:
-        pc = bracket_map[pc]
+        #pc = bracket_map[pc]
+        pc = get_matching_bracket(bracket_map, pc)
 
     pc += 1
 
